ad6e71aa78d8ec73af87155de0ef7c2f92f79560
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-24 00:27:41 +0200

    index name change

---
+++ index1.js
@@ -0,0 +1,110 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBmJqjAXztETX4Dh4vEetlB4QzN9uqReYA",
+  authDomain: "witsmarketproject.firebaseapp.com",
+  databaseURL: "https://witsmarketproject-default-rtdb.firebaseio.com",
+  projectId: "witsmarketproject",
+  storageBucket: "witsmarketproject.appspot.com",
+  messagingSenderId: "650642470600",
+  appId: "1:650642470600:web:49fe3a262e6ca122b597fd",
+  measurementId: "G-EHXK572PE1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth.Auth.Persistence.LOCAL;
+
+window.alert(login("shlomo@gmail.com","123456")); //for testing
+
+function login(email,password){
+  if (email != "shlomo@gmail.com" && password != "123456"){
+      email = document.getElementById("email").value;
+      password = document.getElementById("password").value;
+  }  
+
+  var page = window.location.href;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    //window.alert(user.uid);
+    window.location.href = "index.html";
+  })
+  .catch((error) => {
+    var errorMessage = error.message;
+       window.alert(errorMessage)
+  });
+
+  if (window.location.href != page){
+    return "Success"; 
+  }else{
+    return "Fail"
+  }
+  
+}
+
+function register(){
+  var fName = document.getElementById("fName").value;
+  var lName = document.getElementById("lName").value;
+  var dob = document.getElementById("dob").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var cPassword = document.getElementById("cPassword").value;
+
+  if(password == cPassword){
+    if(fName!= "" && lName != "" && dob != ""){
+      firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user.uid;
+        var rootRef = firebase.database().ref();
+        var usersRef = rootRef.child("users").child(user);
+        var userData = 
+        {
+          firstName: fName,
+          lastName: lName,
+          dateOfBirth: dob,
+          email: email
+        };
+        usersRef.set(userData, function(error){
+          if(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+  
+            window.alert("Message : " + errorMessage);
+          }
+          else{
+            window.location.href = "index.html";
+          }
+        });
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert(errorMessage)
+        // ..
+      });
+    }
+    else{
+      window.alert("Please enter all fields!")
+    }
+    
+  }
+  else{
+    window.alert("Passwords do not match.");
+  }
+}
+
+function logout(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
+
+// document.getElementById("i").innerHTML = 23;
+
+// Code Coverage Testing
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = login;  // Login
+}
+
