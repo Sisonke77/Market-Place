3d9cf7d615441d6b1e6eb75998ca93d83f2cec1e
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 19:04:39 +0200

    1

--- change.js
+++ change.js
@@ -8,25 +8,25 @@ var coins = [200, 100, 50, 20, 10, 5, 2, 1];
  * @example
  * getChange(215, 300); // returns [50, 20, 10, 5]
  */
-function getChange (payable, paid) {
-  var change = [];
-  var length = coins.length;
-  var remaining = paid - payable; // we reduce this below
+// function getChange (payable, paid) {
+//   var change = [];
+//   var length = coins.length;
+//   var remaining = paid - payable; // we reduce this below
 
-  for (var i = 0; i < length; i++) { // loop through array of notes & coins:
-    var coin = coins[i];
+//   for (var i = 0; i < length; i++) { // loop through array of notes & coins:
+//     var coin = coins[i];
 
-    var times_coin_fits = Math.floor(remaining / coin); // no partial coins
-    if(times_coin_fits >= 1) { // check coin fits into the remaining amount
+//     var times_coin_fits = Math.floor(remaining / coin); // no partial coins
+//     if(times_coin_fits >= 1) { // check coin fits into the remaining amount
       
-      for(var j = 0; j < times_coin_fits ; j++) { // add coin to change x times
-        change.push(coin);
-        remaining = remaining - coin;  // subtract coin from remaining
-      }
-    }
-  }
-  return change;
-};
+//       for(var j = 0; j < times_coin_fits ; j++) { // add coin to change x times
+//         change.push(coin);
+//         remaining = remaining - coin;  // subtract coin from remaining
+//       }
+//     }
+//   }
+//   return change;
+// };
 
 function helloWorld(){
   return "Hello world"
@@ -37,5 +37,5 @@ function helloWorld(){
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore next */
 if (typeof module !== 'undefined' && module.exports) {
-   module.exports = getChange;  // allows CommonJS/Node.js require()
+   module.exports = helloWorld;  // allows CommonJS/Node.js require()
 }
