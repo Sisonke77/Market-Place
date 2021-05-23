20fc0a3310a4d375c04ab218309e01bb1c6823c4
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 22:43:36 +0200

    1

--- change.js
+++ change.js
@@ -1,4 +1,3 @@
-var coins = [200, 100, 50, 20, 10, 5, 2, 1];
 /**
  * getChange accepts two parameters (totalPayable and cashPaid) and calculates
  * the change in "coins" that needs to be returned.
@@ -9,30 +8,8 @@ var coins = [200, 100, 50, 20, 10, 5, 2, 1];
  * getChange(215, 300); // returns [50, 20, 10, 5]
  */
 function passwordsEqual (password1, password2) {
-  // var change = [];
-  // var length = coins.length;
-  // var remaining = paid - payable; // we reduce this below
-
-  // for (var i = 0; i < length; i++) { // loop through array of notes & coins:
-  //   var coin = coins[i];
-
-  //   var times_coin_fits = Math.floor(remaining / coin); // no partial coins
-  //   if(times_coin_fits >= 1) { // check coin fits into the remaining amount
-      
-  //     for(var j = 0; j < times_coin_fits ; j++) { // add coin to change x times
-  //       change.push(coin);
-  //       remaining = remaining - coin;  // subtract coin from remaining
-  //     }
-  //   }
-  // }
-  // return change;
   return password1 == password2;
 };
-
-// function helloWorld(){
-//   return 1
-// }
-
 
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
