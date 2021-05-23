d2614bfd4d93d97a05ce848b0a4874d6bca1760b
2153068 <69689058+2153068@users.noreply.github.com>
2021-05-23 18:16:07 +0200

    Update change.js

--- change.js
+++ change.js
@@ -36,7 +36,7 @@ function helloWorld(){
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore next */
-// if (typeof module !== 'undefined' && module.exports) {
-//    module.exports = getChange;  // allows CommonJS/Node.js require()
-//   
-// }
+if (typeof module !== 'undefined' && module.exports) {
+   module.exports = getChange;  // allows CommonJS/Node.js require()
+  
+}
