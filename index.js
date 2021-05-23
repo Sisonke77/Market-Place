6c8092a3bfa931f9bab9c3552cb669ded072e5f0
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 12:40:56 +0200

    1

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.auth.Auth.Persistence.LOCAL;
+
 function login(){
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
