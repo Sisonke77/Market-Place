a5dd27a562c3a91c0b4f71a08ec03086ad6e0c37
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 19:01:54 +0200

    hello world test

--- test.js
+++ test.js
@@ -33,5 +33,11 @@ test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', functio
   assert.deepEqual(result, expected);
 });
 
+test('helloWorld() should return ""Hello world', function(assert) {
+  var result = helloWorld();
+  var expected = "Hello world";
+  assert.deepEqual(result, expected);
+});
+
 /* istanbul ignore next */
 if (typeof module !== 'undefined' && module.exports) { QUnit.load(); } // run the tests
