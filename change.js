fa67e57fa23c24590f90f813b2a8c17c7a5ad665
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 17:23:32 +0200

    1

--- change.js
+++ change.js
@@ -2,10 +2,10 @@ var coins = [200, 100, 50, 20, 10, 5, 2, 1];
 /**
  * getChange accepts two parameters (totalPayable and cashPaid) and calculates
  * the change in "coins" that needs to be returned.
-//  * @param {number} payable the integer amount (in pennies) payable (to be paid)
-//  * @param {number} paid the integer amount (in pennies) the person paid
-//  * @returns {array} change the list of coins we need to dispense to the person
-//  * @example
+ * @param {number} payable the integer amount (in pennies) payable (to be paid)
+ * @param {number} paid the integer amount (in pennies) the person paid
+ * @returns {array} change the list of coins we need to dispense to the person
+ * @example
  * getChange(215, 300); // returns [50, 20, 10, 5]
  */
 function getChange (payable, paid) {
@@ -32,6 +32,6 @@ function getChange (payable, paid) {
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore next */
-if (typeof module !== 'undefined' && module.exports) {
-   module.exports = getChange;  // allows CommonJS/Node.js require()
-}
+// if (typeof module !== 'undefined' && module.exports) {
+//    module.exports = getChange;  // allows CommonJS/Node.js require()
+// }
--- index.js
+++ index.js
@@ -1,136 +1,98 @@
-var coins = [200, 100, 50, 20, 10, 5, 2, 1];
-/**
- * getChange accepts two parameters (totalPayable and cashPaid) and calculates
- * the change in "coins" that needs to be returned.
- * @param {number} payable the integer amount (in pennies) payable (to be paid)
- * @param {number} paid the integer amount (in pennies) the person paid
- * @returns {array} change the list of coins we need to dispense to the person
- * @example
- * getChange(215, 300); // returns [50, 20, 10, 5]
- */
-function getChange (payable, paid) {
-  var change = [];
-  var length = coins.length;
-  var remaining = paid - payable; // we reduce this below
 
-  for (var i = 0; i < length; i++) { // loop through array of notes & coins:
-    var coin = coins[i];
-
-    var times_coin_fits = Math.floor(remaining / coin); // no partial coins
-    if(times_coin_fits >= 1) { // check coin fits into the remaining amount
-      
-      for(var j = 0; j < times_coin_fits ; j++) { // add coin to change x times
-        change.push(coin);
-        remaining = remaining - coin;  // subtract coin from remaining
-      }
-    }
-  }
-  return change;
+var firebaseConfig = {
+  apiKey: "AIzaSyBmJqjAXztETX4Dh4vEetlB4QzN9uqReYA",
+  authDomain: "witsmarketproject.firebaseapp.com",
+  databaseURL: "https://witsmarketproject-default-rtdb.firebaseio.com",
+  projectId: "witsmarketproject",
+  storageBucket: "witsmarketproject.appspot.com",
+  messagingSenderId: "650642470600",
+  appId: "1:650642470600:web:49fe3a262e6ca122b597fd",
+  measurementId: "G-EHXK572PE1"
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.auth.Auth.Persistence.LOCAL;
 
+function login(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-/* The code block below ONLY Applies to Node.js - This Demonstrates
-   re-useability of JS code in both Back-end and Front-end! #isomorphic */
-/* istanbul ignore next */
-if (typeof module !== 'undefined' && module.exports) {
-   module.exports = getChange;  // allows CommonJS/Node.js require()
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    //window.alert(user.uid);
+    window.location.href = "index.html";
+  })
+  .catch((error) => {
+    var errorMessage = error.message;
+    window.alert(errorMessage)
+  });
 }
 
+function register(){
+  var fName = document.getElementById("fName").value;
+  var lName = document.getElementById("lName").value;
+  var dob = document.getElementById("dob").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var cPassword = document.getElementById("cPassword").value;
 
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBmJqjAXztETX4Dh4vEetlB4QzN9uqReYA",
-//   authDomain: "witsmarketproject.firebaseapp.com",
-//   databaseURL: "https://witsmarketproject-default-rtdb.firebaseio.com",
-//   projectId: "witsmarketproject",
-//   storageBucket: "witsmarketproject.appspot.com",
-//   messagingSenderId: "650642470600",
-//   appId: "1:650642470600:web:49fe3a262e6ca122b597fd",
-//   measurementId: "G-EHXK572PE1"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-// firebase.auth.Auth.Persistence.LOCAL;
-
-// function login(){
-//   var email = document.getElementById("email").value;
-//   var password = document.getElementById("password").value;
-
-//   firebase.auth().signInWithEmailAndPassword(email, password)
-//   .then((userCredential) => {
-//     // Signed in
-//     var user = userCredential.user;
-//     //window.alert(user.uid);
-//     window.location.href = "index.html";
-//   })
-//   .catch((error) => {
-//     var errorMessage = error.message;
-//     window.alert(errorMessage)
-//   });
-// }
-
-// function register(){
-//   var fName = document.getElementById("fName").value;
-//   var lName = document.getElementById("lName").value;
-//   var dob = document.getElementById("dob").value;
-//   var email = document.getElementById("email").value;
-//   var password = document.getElementById("password").value;
-//   var cPassword = document.getElementById("cPassword").value;
-
-//   if(password == cPassword){
-//     if(fName!= "" && lName != "" && dob != ""){
-//       firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
-//         // Signed in 
-//         var user = userCredential.user.uid;
-//         var rootRef = firebase.database().ref();
-//         var usersRef = rootRef.child("users").child(user);
-//         var userData = 
-//         {
-//           firstName: fName,
-//           lastName: lName,
-//           dateOfBirth: dob,
-//           email: email
-//         };
-//         usersRef.set(userData, function(error){
-//           if(error){
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
+  if(password == cPassword){
+    if(fName!= "" && lName != "" && dob != ""){
+      firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user.uid;
+        var rootRef = firebase.database().ref();
+        var usersRef = rootRef.child("users").child(user);
+        var userData = 
+        {
+          firstName: fName,
+          lastName: lName,
+          dateOfBirth: dob,
+          email: email
+        };
+        usersRef.set(userData, function(error){
+          if(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
   
-//             window.alert("Message : " + errorMessage);
-//           }
-//           else{
-//             window.location.href = "index.html";
-//           }
-//         });
-//       })
-//       .catch((error) => {
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         window.alert(errorMessage)
-//         // ..
-//       });
-//     }
-//     else{
-//       window.alert("Please enter all fields!")
-//     }
+            window.alert("Message : " + errorMessage);
+          }
+          else{
+            window.location.href = "index.html";
+          }
+        });
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert(errorMessage)
+        // ..
+      });
+    }
+    else{
+      window.alert("Please enter all fields!")
+    }
     
-//   }
-//   else{
-//     window.alert("Passwords do not match.");
-//   }
-// }
+  }
+  else{
+    window.alert("Passwords do not match.");
+  }
+}
 
-// function logout(){
-//   firebase.auth().signOut().then(function() {
-//     // Sign-out successful.
-//   }).catch(function(error) {
-//     // An error happened.
-//   });
-// }
+function logout(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
 
-// // document.getElementById("i").innerHTML = 23;
+// document.getElementById("i").innerHTML = 23;
 
-// // Code Coverage Testing
-// if (typeof module !== 'undefined' && module.exports) {
-//   module.exports = logout;  // Login
-// }
+// Code Coverage Testing
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = logout;  // Login
+}
 
