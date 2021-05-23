0e103e346027ddfe9c3b411a3d141e47157bb5e9
2153068 <69689058+2153068@users.noreply.github.com>
2021-05-23 11:50:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.auth.Auth.Persistence.LOCAL;
-
+// testing
 function login(){
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
