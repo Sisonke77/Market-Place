7b8eba71efe533f259ab7dfded4cf33552635353
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-23 20:46:14 +0200

    1

--- change.js
+++ change.js
@@ -2,10 +2,7 @@ var coins = [200, 100, 50, 20, 10, 5, 2, 1];
 /**
  * getChange accepts two parameters (totalPayable and cashPaid) and calculates
  * the change in "coins" that needs to be returned.
- * @param {number} payable the integer amount (in pennies) payable (to be paid)
- * @param {number} paid the integer amount (in pennies) the person paid
- * @returns {array} change the list of coins we need to dispense to the person
- * @example
+
  * getChange(215, 300); // returns [50, 20, 10, 5]
  */
 function getChange (payable, paid) {
