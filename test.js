f4154383488d3cf407d188e733ae31d4bd7e71b5
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-24 02:10:06 +0200

    1

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ if (typeof module !== 'undefined' && module.exports) { // check we're server-sid
   // alias the QUnit.test method so we don't have to change all our tests
   var test = QUnit.test; // stores a copy of QUnit.test
   require('qunit-tap')(QUnit, console.log); // use console.log for test output
-  var passwordsEqual = require('./change.js'); // load our passwordsEqual method
+  // var passwordsEqual = require('./change.js'); // load our passwordsEqual method
+  var passwordsEqual = require('./index.js'); // load our passwordsEqual method
 }
 
  test('passwordsEqual(12, 12) should return true', function(assert) {
