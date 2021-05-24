e3860ea01fb78323db4e895a5ec20fa684ccbc78
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-24 15:55:49 +0200

    1

--- index.js
+++ index.js
@@ -128,13 +128,13 @@ function logout(){
 //    module.exports = passwordsEqual;  // allows CommonJS/Node.js require()
 // }
 
-function passwordsEqual (password1, password2) {
-  return password1 == password2;
-};
+// function passwordsEqual (password1, password2) {
+//   return password1 == password2;
+// };
 
-/* The code block below ONLY Applies to Node.js - This Demonstrates
-   re-useability of JS code in both Back-end and Front-end! #isomorphic */
-/* istanbul ignore next */
-if (typeof module !== 'undefined' && module.exports) {
-   module.exports = passwordsEqual;  // allows CommonJS/Node.js require()
-}
+// /* The code block below ONLY Applies to Node.js - This Demonstrates
+//    re-useability of JS code in both Back-end and Front-end! #isomorphic */
+// /* istanbul ignore next */
+// if (typeof module !== 'undefined' && module.exports) {
+//    module.exports = passwordsEqual;  // allows CommonJS/Node.js require()
+// }
