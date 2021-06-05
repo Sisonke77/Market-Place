86113ca9e46cf6cf803516aed0e2789327bd0436
Shlomo Brill <2153068@students.wits.ac.za>
2021-06-05 19:21:28 +0200

    1

--- test.js
+++ test.js
@@ -15,16 +15,16 @@ if (typeof module !== 'undefined' && module.exports) { // check we're server-sid
 // });
 
 //passwords matching
- test('passwordsEqual(12, 12) should return true', function(assert) {
-  var result = passwordsEqual(12, 12);
-  var expected = true;
-  assert.deepEqual(result, expected);
-});
+//  test('passwordsEqual(12, 12) should return true', function(assert) {
+//   var result = passwordsEqual(12, 12);
+//   var expected = true;
+//   assert.deepEqual(result, expected);
+// });
 
-test('passwordsEqual(12, 4) should return false', function(assert) {
-  var result = passwordsEqual(12, 4);
-  var expected = false;
-  assert.deepEqual(result, expected);
-});
+// test('passwordsEqual(12, 4) should return false', function(assert) {
+//   var result = passwordsEqual(12, 4);
+//   var expected = false;
+//   assert.deepEqual(result, expected);
+// });
 
 if (typeof module !== 'undefined' && module.exports) { QUnit.load(); } // run the tests
